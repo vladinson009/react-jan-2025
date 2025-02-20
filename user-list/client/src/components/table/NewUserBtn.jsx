706532785fe9185498eadcefac22f12db82f8d4e
vladinSon <vladinson009@icloud.com>
2025-02-20 23:45:33 +0100

    #

--- user-list/client/src/components/table/NewUserBtn.jsx
+++
@@ -1,6 +0,0 @@
-export default function NewUserBtn() {
-
-    return (
-        <button className="btn-add btn">Add new user</button>
-    )
-}
--- user-list/client/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './static/styles.css'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
