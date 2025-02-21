a7a1cfa7725cebee29e300dd8449ce5f048a9dae
vladinSon <vladinson009@icloud.com>
2025-02-21 23:54:54 +0100

    created main section

--- user-list/client/src/components/main/Main.jsx
+++ user-list/client/src/components/main/Main.jsx
@@ -1,3 +1,7 @@
+import Pagination from "./pagination/Pagination";
+import SearchBar from "./search/SearchBar";
+import TableComponent from "./table/TableComponent";
+
 export default function Main() {
 
     return (
@@ -5,16 +9,13 @@ export default function Main() {
             {/* <!-- Section component  --> */}
             <section className="card users-container">
                 {/* <!-- Search bar component --> */}
-
-
+                <SearchBar />
                 {/* <!-- Table component --> */}
-
-
+                <TableComponent />
                 {/* <!-- New user button  --> */}
                 <button className="btn-add btn">Add new user</button>
-
                 {/* <!-- Pagination component  --> */}
-
+                <Pagination />
             </section>
 
 
