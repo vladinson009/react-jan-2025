9bd11ec6270080a48e2b1d0498247e4350bd578f
vladinSon <vladinson009@icloud.com>
2025-03-03 23:49:55 +0100

    added more context provider

--- user-list/client/src/hooks/ContextProvider.jsx
+++ user-list/client/src/hooks/ContextProvider.jsx
@@ -7,12 +7,17 @@ export function MyProvider({ children }) {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isNoContent, setIsNoContent] = useState(false);
+  const [pageSize, setPageSize] = useState(5)
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <MyContext.Provider value={{
       users, setUsers,
       isLoading, setIsLoading,
       isError, setIsError,
-      isNoContent, setIsNoContent
+      isNoContent, setIsNoContent,
+      pageSize, setPageSize,
+      currentPage, setCurrentPage
+
     }}>{children}</MyContext.Provider>
   );
 }
