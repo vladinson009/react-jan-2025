6782dddf1884ad0ea96fca652b18ce81f8da9883
vladinSon <vladinson009@icloud.com>
2025-03-03 20:52:33 +0100

    set isNoContent in context provider

--- user-list/client/src/hooks/ContextProvider.jsx
+++ user-list/client/src/hooks/ContextProvider.jsx
@@ -6,7 +6,13 @@ export function MyProvider({ children }) {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isNoContent, setIsNoContent] = useState(false);
   return (
-    <MyContext.Provider value={{ users, setUsers, isLoading, setIsLoading, isError, setIsError }}>{children}</MyContext.Provider>
+    <MyContext.Provider value={{
+      users, setUsers,
+      isLoading, setIsLoading,
+      isError, setIsError,
+      isNoContent, setIsNoContent
+    }}>{children}</MyContext.Provider>
   );
 }
