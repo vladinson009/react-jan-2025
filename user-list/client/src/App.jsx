24d2b1fefeccacf0184352f416791189d012693d
vladinSon <vladinson009@icloud.com>
2025-03-03 20:13:38 +0100

    added context provider

--- user-list/client/src/App.jsx
+++ user-list/client/src/App.jsx
@@ -1,15 +1,16 @@
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Main from "./components/main/Main"
+import { MyProvider } from "./hooks/ContextProvider"
 
 function App() {
 
   return (
-    <>
+    <MyProvider>
       <Header />
       <Main />
       <Footer />
-    </>
+    </MyProvider>
   )
 }
 
