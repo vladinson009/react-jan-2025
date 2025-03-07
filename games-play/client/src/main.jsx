60a080bb8ca5af6984a4e30aeac24c033f1c5cfb
vladinSon <vladinson009@icloud.com>
2025-03-07 22:16:26 +0100

    imported css and added BrouserRouter to main file

--- games-play/client/src/main.jsx
+++ games-play/client/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
+import App from './App.jsx'
+import './assets/styles/style.css'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+  // {/* </StrictMode>, */ }
 )
