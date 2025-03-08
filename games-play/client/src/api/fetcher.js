56a7daee77bb35490cb72c8bb5d67b2d172e7ec8
vladinSon <vladinson009@icloud.com>
2025-03-08 14:11:21 +0100

    fixed bug

--- games-play/client/src/api/fetcher.js
+++ games-play/client/src/api/fetcher.js
@@ -1,4 +1,4 @@
-import { getUserData } from '../utils/userData';
+import { clearUserData, getUserData } from '../utils/userData';
 
 const host = 'http://localhost:3030';
 
@@ -21,9 +21,13 @@ async function fetcher(url, method, data) {
       if (response.status == 204) {
         return response;
       }
+      if (response.status == 403) {
+        clearUserData();
+      }
       const error = await response.json();
       throw new Error(error.message);
     }
+    return response.json();
   } catch (error) {
     throw error.message;
   }
@@ -33,7 +37,7 @@ function get(url) {
   return fetcher(url, 'get');
 }
 function post(url, data) {
-  return fetcher(url, 'post', data);
+  return fetcher(url, 'POST', data);
 }
 function put(url, data) {
   return fetcher(url, 'put', data);
