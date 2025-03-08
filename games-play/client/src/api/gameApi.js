f0d4d80d41671bc06c31a3d681baff13f02ba2dd
vladinSon <vladinson009@icloud.com>
2025-03-08 19:24:32 +0100

    implement more functionality in game api

--- games-play/client/src/api/gameApi.js
+++ games-play/client/src/api/gameApi.js
@@ -1,4 +1,4 @@
-import { get } from './fetcher';
+import { del, get, post, put } from './fetcher';
 
 function getAll() {
   return get('/data/games?sortBy=_createdOn%20desc');
@@ -9,4 +9,40 @@ function getLatestGame() {
 function getById(gameId) {
   return get('/data/games/' + gameId);
 }
-export default { getAll, getLatestGame, getById };
+function create(userInput) {
+  for (const key in userInput) {
+    if (userInput[key] == '') {
+      const name = key.at(0).toUpperCase() + key.slice(1);
+      throw new Error(`${name} field is required!`);
+    }
+  }
+  return post('/data/games', userInput);
+}
+function editById(gameId, userInput) {
+  for (const key in userInput) {
+    if (userInput[key] == '') {
+      const name = key.at(0).toUpperCase() + key.slice(1);
+      throw new Error(`${name} field is required!`);
+    }
+  }
+  return put('/data/games/' + gameId, userInput);
+}
+function deleteById(gameId) {
+  return del('/data/games/' + gameId);
+}
+function getCommentsById(gameId) {
+  return get(`/data/comments?where=gameId%3D%22${gameId}%22`);
+}
+function createComment(comment, gameId) {
+  return post('/data/comments', { comment, gameId });
+}
+export default {
+  getAll,
+  getLatestGame,
+  getById,
+  create,
+  editById,
+  deleteById,
+  getCommentsById,
+  createComment,
+};
