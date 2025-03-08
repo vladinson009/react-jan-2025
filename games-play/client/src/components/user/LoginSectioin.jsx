626382e4c0c18e26ddfae7af33087f28ad48ebc8
vladinSon <vladinson009@icloud.com>
2025-03-08 14:12:09 +0100

    section functionality

--- games-play/client/src/components/user/LoginSectioin.jsx
+++ games-play/client/src/components/user/LoginSectioin.jsx
@@ -1,11 +1,39 @@
-export default function LoginSection() {
+import { useContext, useState } from "react"
+import { Link } from "react-router-dom";
+import userApi from "../../api/userApi";
+import { setUserData } from "../../utils/userData";
+import context from "../../context/userContext";
 
+
+export default function LoginSection() {
+    const { setUserSession, navigate } = useContext(context)
+    const [error, isError] = useState(false);
+    const [isPassword, setIsPassword] = useState('');
+    function onSubmit(e) {
+        e.preventDefault();
+        const userInput = Object.fromEntries(new FormData(e.target));
+        userApi.login(userInput).then(response => {
+            const userData = {
+                _id: response._id,
+                email: response.email,
+                accessToken: response.accessToken
+            }
+            setUserData(userData);
+            setUserSession(userData);
+            e.target.reset();
+            navigate('/');
+        }).catch(err => {
+            isError(err)
+            setIsPassword('');
+        })
+    }
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form onSubmit={onSubmit} id="login">
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
                     <label htmlFor="email">Email:</label>
                     <input
                         type="email"
@@ -15,10 +43,10 @@ export default function LoginSection() {
                     />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input type="password" id="login-password" name="password" value={isPassword} onChange={(e) => setIsPassword(e.target.value)} />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
+                        <span>If you don't have profile click <Link to="/users/register">here</Link></span>
                     </p>
                 </div>
             </form>
--- games-play/client/src/components/user/RegisterSection.jsx
+++ games-play/client/src/components/user/RegisterSection.jsx
@@ -1,33 +1,58 @@
-export default function RegisterSection() {
+import { useActionState, useContext } from "react";
+import userApi from "../../api/userApi";
+import context from "../../context/userContext";
+import { Link } from "react-router-dom";
 
+export default function RegisterSection() {
+    const initialState = { email: '', password: '', 'confirm-password': '' }
+    const [state, formAction, isPending] = useActionState(onRegister, initialState);
+    const { setUserSession, navigate } = useContext(context)
+
+    async function onRegister(previousState, formData) {
+        const userInput = Object.fromEntries(formData);
+        try {
+            const user = await userApi.register(userInput)
+            const userData = {
+                _id: user._id,
+                email: user.email,
+                accessToken: user.accessToken,
+            }
+            setUserSession(userData);
+            setUserSession(userData);
+            navigate('/');
+        } catch (error) {
+            return { userInput, error }
+        }
+
+    }
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form action={formAction} id="register">
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
-
+                    {state?.error && <p style={{ color: 'red' }}>{state.error}</p>}
                     <label htmlFor="email">Email:</label>
                     <input
                         type="email"
                         id="email"
                         name="email"
                         placeholder="maria@email.com"
+                        defaultValue={state?.userInput?.email}
                     />
-
                     <label htmlFor="pass">Password:</label>
                     <input type="password" name="password" id="register-password" />
 
                     <label htmlFor="con-pass">Confirm Password:</label>
                     <input type="password" name="confirm-password" id="confirm-password" />
 
-                    <input className="btn submit" type="submit" value="Register" />
+                    <input disabled={isPending} className="btn submit" type="submit" value="Register" />
 
                     <p className="field">
-                        <span>If you already have profile click <a href="#">here</a></span>
+                        <span>If you already have profile click <Link to="/users/login">here</Link></span>
                     </p>
                 </div>
             </form>
-        </section>
+        </section >
     )
 }
