c7a6dc266ac840ec87c8889ddc44ce225f41e8cc
vladinSon <vladinson009@icloud.com>
2025-03-08 14:11:56 +0100

    dynamic navigationi

--- games-play/client/src/components/header/Header.jsx
+++ games-play/client/src/components/header/Header.jsx
@@ -1,28 +1,34 @@
 import { NavLink } from "react-router-dom"
+import { useContext } from "react";
+import context from "../../context/userContext";
 
 function isActive({ isActive }) {
-    console.log(isActive);
-
     return { color: isActive && "rgb(82, 133, 139)" }
 
 }
 export default function Header() {
+    const { userSession } = useContext(context)
+
+
     return (
         <header>
             {/* <!-- Navigation --> */}
             <h1><NavLink style={isActive} className="home" to="/">GamesPlay</NavLink></h1>
             <nav>
                 <NavLink style={isActive} to="/games/catalogue">All games</NavLink>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <NavLink style={isActive} to="/games/create">Create Game</NavLink>
-                    <NavLink style={isActive} to="/users/logout">Logout</NavLink>
-                </div>
-                {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <NavLink style={isActive} to="/users/login">Login</NavLink>
-                    <NavLink style={isActive} to="/users/register">Register</NavLink>
-                </div>
+                {userSession?.accessToken
+                    ? <div id="user">
+                        {/* <!-- Logged-in users --> */}
+                        <NavLink style={isActive} to="/games/create">Create Game</NavLink>
+                        <NavLink style={isActive} to="/users/logout">Logout</NavLink>
+                    </div>
+                    : <div id="guest">
+                        {/* <!-- Guest users --> */}
+                        <NavLink style={isActive} to="/users/login">Login</NavLink>
+                        <NavLink style={isActive} to="/users/register">Register</NavLink>
+                    </div>}
+
+
             </nav>
         </header>
 
